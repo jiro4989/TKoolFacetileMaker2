696200bc011663a451986b5f9ee613155c987d76
jiro4989 <jiroron666@gmail.com>
2020-05-03 13:39:19 +0000

    fix script

--- link.sh
+++ link.sh
@@ -3,9 +3,10 @@
 set -eu
 
 readonly modules="$(cat modules.txt | tr \\n ,)"
+readonly module_path="${1:-/usr/lib/jvm/javafx-jmods-11.0.2/}"
 
 jlink \
-  --module-path "/usr/lib/jvm/javafx-jmods-11.0.2/" \
+  --module-path "$module_path" \
   --add-modules "$modules" \
   --compress=2 \
   --output jre
