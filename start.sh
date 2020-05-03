7791eb62db1c0f0bd1fe8c0409447f9a62c78455
jiro4989 <jiroron666@gmail.com>
2020-05-03 10:58:44 +0000

    add script

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java --module-path /usr/lib/jvm/javafx-sdk-11.0.2/lib --add-modules javafx.base,javafx.controls,javafx.swing,javafx.graphics,javafx.fxml -jar build/libs/TKoolFacetileMaker2.jar
