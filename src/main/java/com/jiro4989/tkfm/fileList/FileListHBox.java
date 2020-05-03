015d0121605cdf2fb0d4732d22521e9e7418a934
jiro4989 <jiroron666@gmail.com>
2020-05-03 12:29:09 +0000

    Format code

--- src/main/java/com/jiro4989/tkfm/fileList/FileListHBox.java
+++ src/main/java/com/jiro4989/tkfm/fileList/FileListHBox.java
@@ -1,31 +1,30 @@
 package com.jiro4989.tkfm.fileList;
 
-import java.io.IOException;
-
 import com.jiro4989.tkfm.MainController;
+import java.io.IOException;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.HBox;
 
-public class FileListHBox extends HBox{
-	private MainController mainController;
-	private FileListHBoxController controller;
+public class FileListHBox extends HBox {
+  private MainController mainController;
+  private FileListHBoxController controller;
 
-	public FileListHBox(MainController aMainController) {
-		super();
-		mainController = aMainController;
+  public FileListHBox(MainController aMainController) {
+    super();
+    mainController = aMainController;
 
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("FileListHBox.fxml"));
-			HBox root = (HBox) loader.load();
-			controller = (FileListHBoxController) loader.getController();
-			controller.setMainController(mainController);
-			getChildren().add(root);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("FileListHBox.fxml"));
+      HBox root = (HBox) loader.load();
+      controller = (FileListHBoxController) loader.getController();
+      controller.setMainController(mainController);
+      getChildren().add(root);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 
-	public FileListHBoxController getController() {
-		return controller;
-	}
+  public FileListHBoxController getController() {
+    return controller;
+  }
 }
--- src/main/java/com/jiro4989/tkfm/outputViewer/MyButton.java
+++ src/main/java/com/jiro4989/tkfm/outputViewer/MyButton.java
@@ -5,32 +5,32 @@ import javafx.scene.control.Button;
 
 /**
  * 自身のグリッドペインでの行列番号を保持するだけの拡張クラス。
- * @author jiro
  *
+ * @author jiro
  */
-public class MyButton extends Button{
-	private final int row;
-	private final int column;
+public class MyButton extends Button {
+  private final int row;
+  private final int column;
 
-	public MyButton(String title, int aRow, int aColumn) {
-		super(title);
+  public MyButton(String title, int aRow, int aColumn) {
+    super(title);
 
-		row = aRow;
-		column = aColumn;
-		setStyle("-fx-font-size: 28pt; -fx-base: #ffffff;");
-		setPadding(new Insets(0,0,0,0));
-	}
-	
-	int getRow() {
-		return row;
-	}
-	
-	int getColumn() {
-		return column;
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("title: %s, row: %d, column: %d", getText(), row, column);
-	}
+    row = aRow;
+    column = aColumn;
+    setStyle("-fx-font-size: 28pt; -fx-base: #ffffff;");
+    setPadding(new Insets(0, 0, 0, 0));
+  }
+
+  int getRow() {
+    return row;
+  }
+
+  int getColumn() {
+    return column;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("title: %s, row: %d, column: %d", getText(), row, column);
+  }
 }
