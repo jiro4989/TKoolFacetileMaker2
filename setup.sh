76acf314eca1c4fef4cf4f586c6e27c099a8ac7d
jiro <jiroron666@gmail.com>
2020-05-04 21:19:43 +0900

    Java11からJava14にアップグレードした #4 (#18)
    * add setup script
    
    * upgrade javafx version
    
    * upgrade javafx version

--- setup.sh
+++ setup.sh
@@ -2,4 +2,9 @@
 
 set -eux
 
-sudo apt install -y openjdk-11-jdk
+curl -O https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_linux-x64_bin.tar.gz
+tar xvf openjdk-14_linux-x64_bin.tar.gz
+
+mkdir -p /opt/java
+mv jdk-14 /opt/java/
+ln -sfn /opt/java/jdk-14 /opt/java/current
