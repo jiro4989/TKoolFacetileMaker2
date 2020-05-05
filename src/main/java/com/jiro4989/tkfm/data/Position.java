3eded557ea3071ad869236d6e6a98d746de4cd47
jiro4989 <jiroron666@gmail.com>
2020-05-05 08:37:22 +0000

    Add properties

--- src/main/java/com/jiro4989/tkfm/data/Position.java
+++ src/main/java/com/jiro4989/tkfm/data/Position.java
@@ -1,19 +1,38 @@
 package com.jiro4989.tkfm.data;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Position {
-  private int x = 0;
-  private int y = 0;
+  private IntegerProperty x = new SimpleIntegerProperty(0);
+  private IntegerProperty y = new SimpleIntegerProperty(0);
 
   public Position(int x, int y) {
-    this.x = x;
-    this.y = y;
+    this.x = new SimpleIntegerProperty(x);
+    this.y = new SimpleIntegerProperty(y);
   }
 
-  public int getX() {
+  public IntegerProperty xProperty() {
     return x;
   }
 
-  public int getY() {
+  public IntegerProperty yProperty() {
     return y;
+  }
+
+  public int getX() {
+    return x.get();
+  }
+
+  public int getY() {
+    return y.get();
+  }
+
+  public void setX(int x) {
+    this.x.set(x);
+  }
+
+  public void setY(int y) {
+    this.y.set(y);
   }
 }
--- src/main/java/com/jiro4989/tkfm/data/Rectangle.java
+++ src/main/java/com/jiro4989/tkfm/data/Rectangle.java
@@ -1,19 +1,38 @@
 package com.jiro4989.tkfm.data;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Rectangle {
-  private int width = 0;
-  private int height = 0;
+  private IntegerProperty width = new SimpleIntegerProperty(0);
+  private IntegerProperty height = new SimpleIntegerProperty(0);
 
   public Rectangle(int w, int h) {
-    this.width = w;
-    this.height = h;
+    this.width = new SimpleIntegerProperty(w);
+    this.height = new SimpleIntegerProperty(h);
   }
 
-  public int getWidth() {
+  public IntegerProperty widthProperty() {
     return width;
   }
 
-  public int getHeight() {
+  public IntegerProperty heightProperty() {
     return height;
+  }
+
+  public int getWidth() {
+    return width.get();
+  }
+
+  public int getHeight() {
+    return height.get();
+  }
+
+  public void setWidth(int w) {
+    this.width.set(w);
+  }
+
+  public void setHeight(int h) {
+    this.height.set(h);
   }
 }
