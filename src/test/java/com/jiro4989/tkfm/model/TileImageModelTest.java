3f357b7eb8f20f5d7dbce57a05b7550d2b7571f2
jiro4989 <jiroron666@gmail.com>
2020-05-09 08:59:16 +0000

    Add test code

--- src/test/java/com/jiro4989/tkfm/model/TileImageModelTest.java
+++ src/test/java/com/jiro4989/tkfm/model/TileImageModelTest.java
@@ -30,4 +30,22 @@ public class TileImageModelTest {
       }
     }
   }
+
+  @ParameterizedTest
+  @CsvSource({
+    "0", "1", "2", "3", "4", "5", "6", "7", "8",
+  })
+  public void testBulkInsert(int index) throws Exception {
+    var t = new TileImageModel();
+    var images = new LinkedList<Image>();
+    for (int i = 0; i < 8; i++) images.add(new Image("20x20.png"));
+
+    switch (index) {
+      case 0:
+        t.bulkInsert(images);
+        break;
+      default:
+        t.bulkInsert(images, index);
+    }
+  }
 }
