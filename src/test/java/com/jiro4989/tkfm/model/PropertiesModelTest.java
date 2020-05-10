af56998720fb29a6bfc4f8fe2ad5501be26a3b55
jiro4989 <jiroron666@gmail.com>
2020-05-10 03:31:20 +0000

    PropertiesModelにテストケースを追加 #5

--- src/test/java/com/jiro4989/tkfm/model/PropertiesModelTest.java
+++ src/test/java/com/jiro4989/tkfm/model/PropertiesModelTest.java
@@ -1,12 +1,13 @@
 package com.jiro4989.tkfm.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
 import org.junit.jupiter.api.Test;
 
 public class PropertiesModelTest {
   @Test
-  public void testLoadStore() {
+  public void testLoadStoreWindow() {
     var p = new PropertiesModel.Window("test_window");
     p.setX(100.1);
     p.setY(200.2);
@@ -19,5 +20,20 @@ public class PropertiesModelTest {
     assertEquals(200.2, p.getY());
     assertEquals(300.3, p.getWidth());
     assertEquals(400.4, p.getHeight());
+  }
+
+  @Test
+  public void testLoadStoreChosedFile() {
+    var p = new PropertiesModel.ChoosedFile("test_choosed_file");
+    // 存在するファイルなら何でも良い
+    p.setOpenedFile(new File(".github/workflows/main.yml"));
+    p.setSavedFile(new File("src/main/java/com/jiro4989/tkfm/Main.java"));
+    p.store();
+
+    p.load();
+    assertTrue(p.getOpenedFile().isPresent());
+    assertEquals("main.yml", p.getOpenedFile().get().getName());
+    assertTrue(p.getSavedFile().isPresent());
+    assertEquals("Main.java", p.getSavedFile().get().getName());
   }
 }
