e0d42153a6c6360b56cd644f924211ec8c8218cb
jiro4989 <jiroron666@gmail.com>
2021-12-26 17:58:01 +0900

    ImageFormatStageをKotlinで書き直した

---
+++ src/main/kotlin/com/jiro4989/tkfm/ImageFormatStage.kt
@@ -0,0 +1,33 @@
+package com.jiro4989.tkfm
+
+import com.jiro4989.tkfm.controller.ImageFormatViewController
+import com.jiro4989.tkfm.model.ImageFormatModel
+import javafx.fxml.FXMLLoader
+import javafx.scene.*
+import javafx.scene.layout.*
+import javafx.stage.*
+
+class ImageFormatStage : Stage {
+  private lateinit var controller: ImageFormatViewController
+
+  constructor() {
+    try {
+      val loader = FXMLLoader(this.javaClass.getResource("fxml/image_format_view.fxml"))
+      val root = loader.load() as VBox
+      controller = loader.getController() as ImageFormatViewController
+
+      // UI conficuration
+      val scene = Scene(root)
+      setScene(scene)
+      initStyle(StageStyle.UTILITY)
+      initModality(Modality.APPLICATION_MODAL)
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
+  }
+
+  fun getImageFormat() = controller.getImageFormat()
+
+  /** Returns ok button was pressed. */
+  fun getOK() = controller.getOK()
+}
