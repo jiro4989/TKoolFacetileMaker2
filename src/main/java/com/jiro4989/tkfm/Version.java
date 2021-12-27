6c8bb7ae4a0a4a61af9d0ed158e8d757d47da6c7
jiro <jiroron666@gmail.com>
2021-12-27 23:52:41 +0900

    chore: バージョン情報をpropertiesファイルから取得するように変更した #62 (#77)
    * chore: Version.javaをKotlinに移植した
    
    * 埋め込む文字列はpropertiesファイルで管理するように変更

--- src/main/java/com/jiro4989/tkfm/Version.java
+++
@@ -1,6 +0,0 @@
-package com.jiro4989.tkfm;
-
-public class Version {
-  public static final String version = "dev";
-  public static final String commitHash = "dev";
-}
--- template/Version.java
+++
@@ -1,6 +0,0 @@
-package com.jiro4989.tkfm;
-
-public class Version {
-  public static final String version = "$version";
-  public static final String commitHash = "$commitHash";
-}
