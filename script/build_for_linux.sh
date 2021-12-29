6026e70973e2a26f640a71b2db011d1b857abdca
jiro <jiroron666@gmail.com>
2021-12-29 09:28:37 +0900

    chore: アプリケーションのバージョン書式を変更 (#82)
    * versionの書式を修正
    
    * versionの書式を修正
    
    * CIのversion書式を修正
    
    * ファイル名のバージョン番号を修正
    
    * versionの書式を修正

--- script/build_for_linux.sh
+++ script/build_for_linux.sh
@@ -2,4 +2,4 @@
 
 set -eux
 
-APP_NAME="tkfm" JAVAFX_VERSION=16 OS_NAME=linux ARCHIVE_CMD="tar czf" ARTIFACT_EXT=.tar.gz ENTRYPOINT_SCRIPT_EXT="" VERSION="dev" ./script/create_artifact.sh
+APP_NAME="tkfm" JAVAFX_VERSION=16 OS_NAME=linux ARCHIVE_CMD="tar czf" ARTIFACT_EXT=.tar.gz ENTRYPOINT_SCRIPT_EXT="" VERSION="0.0.0-SNAPSHOT" ./script/create_artifact.sh
